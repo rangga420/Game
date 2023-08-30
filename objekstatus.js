6dba9a24063e671f0e49083a91abf83d046e2e23
GilbertCB <gilbertchristianbelagcb@gmail.com>
2023-08-30 16:11:32 +0700

    INI OBJECT HUMAN SAMA MONSTER

---
+++ objekstatus.js
@@ -0,0 +1,43 @@
+let human = {
+    health: 10,
+    power: 5,
+    gold: 20
+}
+
+/*
+PAKAI REWARD POWER KALAU SHOPNYA GAJADI
+JADI REWARD BATTLENYA LANGSUNG EXTRA POWER
+*/
+
+//MONSTER--------------------------------
+let goblin = {
+    power: 2,
+    rewardGold: 5,
+    rewardPower: 1
+}
+
+let orc = {
+    power: 5,
+    rewardGold: 7,
+    rewardPower: 2
+}
+
+let undead = {
+    power: 7,
+    rewardGold: 10,
+    rewardPower: 4
+}
+
+/*
+    KHUSUS BOSS ADA HEALTHNYA JADI SELAMA HEALTH BOSS MASIH ADA, BATTLE TERUS SISTEMNYA TURN BASED
+    SAMA KAYAK HUMAN (HEALTH AKHIR = HEALTH - SELISIH POWER)
+*/
+let boss = {
+    health: 15,
+    power: 12,
+}
+//MONSTER--------------------------------
+
+//ITEM-----------------------------------
+
+//ITEM-----------------------------------
