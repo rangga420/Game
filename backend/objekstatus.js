0f7c5e97dde7c1540ad4bb364f8d9e2386990a5e
rangga nurhadiansyah <ranggadiningrat7@gmail.com>
2023-08-31 11:46:18 +0700

    UPDATE JS

--- backend/objekstatus.js
+++ backend/objekstatus.js
@@ -58,22 +58,79 @@ let item = {
 
 
 function render() {
-  let nama = prompt("Silahkan masukkan nama user")
-  let userName = document.getElementById("userName");
-  userName.innerText = nama;
+      let nama = prompt("Silahkan masukkan nama user")
+      let userName = document.getElementById("userName");
+      userName.innerText = nama;
 
-  let health = document.getElementById("health");
-  health.innerText = human.health;
+      let health = document.getElementById("health");
+      health.innerText = human.health;
 
-  let gold = document.getElementById("gold");
-  gold.innerText = human.gold;
+      let gold = document.getElementById("gold");
+      gold.innerText = human.gold;
 
-  let power = document.getElementById("power");
-  power.innerText = human.power;
+      let power = document.getElementById("power");
+      power.innerText = human.power;
+}
+
+// function addImage(){
+//   let image = document.getElementById('fight-right')
+//   console.log(image)
+// }
+let found = false
+// function addImageOnce() {
+//   let image = document.getElementById('fight-right');
+//   let newImage = document.createElement("img");
+//   newImage.src = "../frontend/asset/shop.svg";
+//   image.appendChild(newImage);
+
+//   // Hapus event listener setelah dijalankan
+//   document.getElementById("toggleButton2").removeEventListener("click", addImageOnce);
+//   let buttonElement = document.querySelector("#toggleButton2 button");
+//   let buttonId = buttonElement.getAttribute("id");
+//   console.log("Nilai id dari elemen button:", buttonId);
+// }
+
+// document.getElementById("toggleButton").addEventListener("click", addImageOnce);
+// document.getElementById("toggleButton2").addEventListener("click", addImageOnce);
+
+// document.getElementById("toggleButton").addEventListener("click", function(event) {
+//   if (event.target.tagName === "IMG") {
+//     let buttonId = event.target.parentElement.id;
+//     let fightRight = document.getElementById('fight-right');
+//     fightRight.innerHTML = '';
+//     let newImage = document.createElement("img");
+//     if (buttonId === "1") {
+//       newImage.src = "../frontend/asset/shop.svg";
+//     } else if (buttonId === "2") {
+//       newImage.src = "../frontend/asset/hutan.svg";
+//     }
+//     fightRight.appendChild(newImage);
+//   }
+// });
+
+function handleImageButtonClick(event) {
+  if (event.target.tagName === "IMG") {
+    let buttonId = event.target.parentElement.id;
+    let fightRight = document.getElementById('fight-right');
+    fightRight.innerHTML = '';
+    let newImage = document.createElement("img");
+    if (buttonId === "1") {
+      newImage.src = "../frontend/asset/shop.svg";
+    } else if (buttonId === "2") {
+      newImage.src = "../frontend/asset/hutan.svg";
+    }
+    fightRight.appendChild(newImage);
+  }
+}
+
+function main(){
+  document.getElementById("toggleButton").addEventListener("click", handleImageButtonClick);
 }
 
 window.addEventListener("load", function() {
-  render();
+  // render();
+  // addImage()
+  main()
 });
 
 
